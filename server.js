d1d7ef520177c2ee9de77f470652e22919d0f1c1
AlexWellbelove <up863564@myport.ac.uk>
2019-05-10 00:51:13 +0100

    css shit

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ let enabled = true;
 function toggle(){
   if (enabled) {
     document.getElementById('status').innerHTML = "The unattended display is Disabled";
+    document.getElementById('toggler').innerHTML = "Enable";
     enabled = false;
   }
   else {
     document.getElementById('status').innerHTML = "The unattended display is Enabled";
+    document.getElementById('toggler').innerHTML = "Disable";
     enabled = true;
   }
 }
