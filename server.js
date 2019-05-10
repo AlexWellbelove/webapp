9d89079a9f5a0e59f4627cf079e73f4164531074
AlexWellbelove <up863564@myport.ac.uk>
2019-05-10 03:45:40 +0100

    think i fixed it

--- server.js
+++ server.js
@@ -38,48 +38,52 @@ function previewFiles() {
 
   if (files) {
     [].forEach.call(files, readAndPreview);
-    console.log(document.getElementById("browse").value);
   }
 }
 
-var form = document.getElementById('uploadform');
-var fileSelect = document.getElementById('browse');
-var uploadButton = document.getElementById('submit');
+function send(){
+  var form = document.getElementById('uploadform');
+  var fileSelect = document.getElementById('browse');
+  var uploadButton = document.getElementById('submit');
 
-  if(form){
-  form.addEventListener("submit", event, false);
+    if(form){
+    form.addEventListener("submit", event, false);
 
-    event.preventDefault();
-    // Update button text.
-    uploadButton.innerHTML = 'Uploading...';
-    var files = fileSelect.files;
-    var formData = new FormData();
-    // Loop through each of the selected files.
-    for (var i = 0; i < files.length; i++) {
-      var file = files[i];
+      event.preventDefault();
+      // Update button text.
+      uploadButton.innerHTML = 'Uploading...';
+      var files = fileSelect.files;
+      var formData = new FormData();
+      // Loop through each of the selected files.
+      for (var i = 0; i < files.length; i++) {
+        var file = files[i];
 
-      // Check the file type.
-      if (!file.type.match('image.*')) {
-        continue;
+        // Check the file type.
+        if (!file.type.match('image.*')) {
+          continue;
+        }
+        // Add the file to the request.
+        formData.append('photos[]', file, file.name);
       }
+      formData.append(name, file);
+      var xhr = new XMLHttpRequest();
+      xhr.open('POST', 'handler.php', true);
+      //fuckup occurs here
+      xhr.onload = function () {
 
-      // Add the file to the request.
-      formData.append('photos[]', file, file.name);
+        console.log("got here");
+        if (xhr.status === 200) {
+
+
+      // File(s) uploaded.
+        uploadButton.innerHTML = 'Upload';
+        } else {
+          alert('An error occurred!');
     }
-    formData.append(name, file, filename);
-    var xhr = new XMLHttpRequest();
-    xhr.open('POST', 'handler.php', true);
-
-    xhr.onload = function () {
-      if (xhr.status === 200) {
-    // File(s) uploaded.
-      uploadButton.innerHTML = 'Upload';
-      } else {
-        alert('An error occurred!');
+    }
+    xhr.send(formData);
+    console.log("worked");
+  };
   }
-  xhr.send(formData);
-  console.log("worked");
-};
-}
 
-    // The rest of the code will go here...
+      // The rest of the code will go here...
