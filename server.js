34622c0554eda8512340b3ad49a4d5cdde2a0f8c
AlexWellbelove <up863564@myport.ac.uk>
2019-05-10 02:11:42 +0100

    file upload

--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 let enabled = true;
 
-function toggle(){
+function toggle() {
   if (enabled) {
     document.getElementById('status').innerHTML = "The unattended display is Disabled";
     document.getElementById('toggler').innerHTML = "Enable";
     enabled = false;
-  }
-  else {
+  } else {
     document.getElementById('status').innerHTML = "The unattended display is Enabled";
     document.getElementById('toggler').innerHTML = "Disable";
     enabled = true;
+  }
+}
+
+function previewFiles() {
+
+  var preview = document.querySelector('#preview');
+  var files = document.querySelector('input[type=file]').files;
+
+  function readAndPreview(file) {
+
+    // Make sure `file.name` matches our extensions criteria
+    if (/\.(jpe?g|png|gif)$/i.test(file.name)) {
+      var reader = new FileReader();
+
+      reader.addEventListener("load", function() {
+        var image = new Image();
+        image.height = 100;
+        image.title = file.name;
+        image.src = this.result;
+        preview.appendChild(image);
+      }, false);
+
+      reader.readAsDataURL(file);
+    }
+
+  }
+
+  if (files) {
+    [].forEach.call(files, readAndPreview);
   }
 }
